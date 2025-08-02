84cb113035b701140afd6c1b2f9cfacffaf47b61
Ahmed Abdelaziz <s24513385eg@std.aou.edu.eg>
2025-08-02 14:18:14 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-
-
+  
+  // المهم: علشان الصور والروترات تشتغل بعد الرفع
+  base: './',
 
   server: {
     proxy: {
@@ -17,12 +17,5 @@ export default defineConfig({
         rewrite: path => path.replace(/^\/api/, '')
       }
     }
-  
-}
-
-
-
-
-
-
+  }
 })
