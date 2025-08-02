72dc4aca719f28cf5690ed98938cacb9778aa84b
Ahmed Abdelaziz <s24513385eg@std.aou.edu.eg>
2025-08-02 14:17:08 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 
-import App from './App.jsx'
-import "./styles/app.scss"
+import App from './App.jsx';
+import "./styles/app.scss";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
